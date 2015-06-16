8c73471970e5d58ae75d43de065a2a07ddd007d8
Tom Honeybone <tomhon@hotmail.com>
2015-06-16 16:17:38 -0700

    2nd Commit

--- ContactsListLatest/ContactsListLatest/Controllers/ContactsController.cs
+++ ContactsListLatest/ContactsListLatest/Controllers/ContactsController.cs
@@ -32,6 +32,7 @@ namespace ContactsListLatest.Controllers
                 new Contact { Id = 1, EmailAddress = "barney@contoso.com", Name = "Barney Poland"},
                 new Contact { Id = 2, EmailAddress = "lacy@contoso.com", Name = "Lacy Barrera"},
                 new Contact { Id = 3, EmailAddress = "lora@microsoft.com", Name = "Lora Riggs"},
+                new Contact { Id = 3, EmailAddress = "vivienne@microsoft.com", Name = "Vivienne Stark"},
                 new Contact { Id = 3, EmailAddress = "fiona@microsoft.com", Name = "Fiona Riggs"}
             };
         }
