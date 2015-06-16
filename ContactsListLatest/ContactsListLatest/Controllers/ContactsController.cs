03c9236c7688ad5fceb051823082d5e2dabe097b
tomhon <tomhon@hotmail.com>
2015-06-16 12:23:28 -0700

    1st Commit

---
+++ ContactsListLatest/ContactsListLatest/Controllers/ContactsController.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Net.Http;
+//using System.Web.Http;
+
+//namespace ContactsListLatest.Controllers
+//{
+//    public class ContactsController : ApiController
+//    {
+//    }
+//}
+
+using ContactsListLatest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ContactsListLatest.Controllers
+{
+    public class ContactsController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return new Contact[]{
+                new Contact { Id = 1, EmailAddress = "barney@contoso.com", Name = "Barney Poland"},
+                new Contact { Id = 2, EmailAddress = "lacy@contoso.com", Name = "Lacy Barrera"},
+                new Contact { Id = 3, EmailAddress = "lora@microsoft.com", Name = "Lora Riggs"},
+                new Contact { Id = 3, EmailAddress = "fiona@microsoft.com", Name = "Fiona Riggs"}
+            };
+        }
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] Contact contact)
+        {
+            // todo: save the contact somewhere
+            return Request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
---
+++ ContactsListLatest/ContactsListLatest/Models/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//namespace ContactsListLatest.Models
+//{
+//    public class Contact
+//    {
+//    }
+//}
+
+namespace ContactsListLatest.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
