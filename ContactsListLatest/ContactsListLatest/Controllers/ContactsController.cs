355c97376df35845884ed7e47e90ba1ef4891bd2
Tom Honeybone <tomhon@hotmail.com>
2015-06-17 12:51:31 -0700

    Updated to provide publisher information

--- ContactsListLatest/ContactsListLatest/Controllers/ContactsController.cs
+++ ContactsListLatest/ContactsListLatest/Controllers/ContactsController.cs
@@ -25,18 +25,30 @@ namespace ContactsListLatest.Controllers
 {
     public class ContactsController : ApiController
     {
+        //[HttpGet]
+        //public IEnumerable<Contact> Get()
+        //{
+        //    return new Contact[]{
+        //        new Contact { Id = 1, EmailAddress = "barney@contoso.com", Name = "Barney Poland"},
+        //        new Contact { Id = 2, EmailAddress = "lacy@contoso.com", Name = "Lacy Barrera"},
+        //        new Contact { Id = 3, EmailAddress = "lora@microsoft.com", Name = "Lora Riggs"},
+        //        new Contact { Id = 4, EmailAddress = "laura@microsoft.com", Name = "Laura Means"},
+        //        new Contact { Id = 5, EmailAddress = "vivienne@microsoft.com", Name = "Vivienne Stark"},
+        //        new Contact { Id = 6, EmailAddress = "fiona@microsoft.com", Name = "Fiona Riggs"}
+        //    };
+        //}
+
         [HttpGet]
-        public IEnumerable<Contact> Get()
+        public IEnumerable<Account> GetAccounts()
         {
-            return new Contact[]{
-                new Contact { Id = 1, EmailAddress = "barney@contoso.com", Name = "Barney Poland"},
-                new Contact { Id = 2, EmailAddress = "lacy@contoso.com", Name = "Lacy Barrera"},
-                new Contact { Id = 3, EmailAddress = "lora@microsoft.com", Name = "Lora Riggs"},
-                new Contact { Id = 4, EmailAddress = "laura@microsoft.com", Name = "Laura Means"},
-                new Contact { Id = 5, EmailAddress = "vivienne@microsoft.com", Name = "Vivienne Stark"},
-                new Contact { Id = 6, EmailAddress = "fiona@microsoft.com", Name = "Fiona Riggs"}
+            return new Account[]{
+                new Account { Id = 1, TE = "Matt", Name = "PayPal"},
+                new Account { Id = 2, TE = "Richard", Name = "Netflix"},
+                new Account { Id = 3, TE = "Nathalie", Name = "Twitch"},
+
             };
         }
+
         [HttpPost]
         public HttpResponseMessage Post([FromBody] Contact contact)
         {
--- ContactsListLatest/ContactsListLatest/Models/Contact.cs
+++ ContactsListLatest/ContactsListLatest/Models/Contact.cs
@@ -18,4 +18,11 @@ namespace ContactsListLatest.Models
         public string Name { get; set; }
         public string EmailAddress { get; set; }
     }
+
+    public class Account
+    {
+        public int Id { get; set; }
+        public string TE { get; set; }
+        public string Name { get; set; }
+    }
 }
